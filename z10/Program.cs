4e6265996885bdb96dd4b6e4b45951f1c9709db4
Vilina <vilina.grafova@mail.ru>
2022-11-23 00:16:22 +0300

    z10

---
+++ z10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+
+//решение задачи текстовое
+//Console.WriteLine("Введите трехзначное число");
+//string text = Console.ReadLine();
+//Console.WriteLine(text[1]);
+
+//решение задачи математическое
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+number = (int)(number / 10); // остаются первые две цыфры трёхзначного числа 
+number = number % 10;   // убираем остаток и остаётся центральная цифра
+Console.WriteLine(number);
